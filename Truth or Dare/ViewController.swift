0988778677f2dd85138bc156051bf2a712b7bcfa
Adham <Hameda6256@gmail.com>
2021-07-12 21:53:12 -0400

    Initial Commit

---
+++ Truth or Dare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Truth or Dare
+//
+//  Created by Adham Hamed on 7/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
